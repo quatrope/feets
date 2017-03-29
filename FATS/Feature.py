37898224834ce98575ed8a62d143ad330cb201e4
Juan B Cabral <jbc.develop@gmail.com>
2017-03-29 01:07:41 -0300

    replaced  sys.exit  with exceptions replaced prints for warnings make feature calculation now return numpy arrays

--- FATS/Feature.py
+++ FATS/Feature.py
@@ -158,3 +158,163 @@ class FeatureSpace:
         else:
             return self.__result
 
+import os
+import time
+import inspect
+import warnings
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import featureFunction
+
+
+class FeatureError(Exception): pass
+
+
+class FeatureNotFound(ValueError): pass
+
+
+class FeatureWarning(Warning): pass
+
+
+class FeatureSpace:
+    """
+    This Class is a wrapper class, to allow user select the
+    features based on the available time series vectors (magnitude, time,
+    error, second magnitude, etc.) or specify a list of features.
+
+    __init__ will take in the list of the available data and featureList.
+
+    User could only specify the available time series vectors, which will
+    output all the features that need this data to be calculated.
+
+    User could only specify featureList, which will output
+    all the features in the list.
+
+    User could specify a list of the available time series vectors and
+    featureList, which will output all the features in the List that
+    use the available data.
+
+    Additional parameters are used for individual features.
+    Format is featurename = [parameters]
+
+    usage:
+    data = np.random.randint(0,10000, 100000000)
+    # automean is the featurename and [0,0] is the parameter for the feature
+    a = FeatureSpace(category='all', automean=[0,0])
+    print a.featureList
+    a=a.calculateFeature(data)
+    print a.result(method='array')
+    print a.result(method='dict')
+
+    """
+    def __init__(self, Data=None, featureList=None, excludeList=[], **kwargs):
+        self.featureFunc = []
+        self.featureList = []
+        self.featureOrder = []
+        self.featureList = []
+
+        self.sort = False
+
+        if Data is not None:
+            self.Data = Data
+
+            if self.Data == 'all':
+                if featureList == None:
+
+                    if excludeList == None:
+                        for name, obj in inspect.getmembers(featureFunction):
+                            if inspect.isclass(obj) and name != 'Base':
+                                # if set(obj().Data).issubset(self.Data):
+                                self.featureOrder.append((inspect.getsourcelines(obj)[-1:])[0])
+                                self.featureList.append(name)
+                    else:
+                        for name, obj in inspect.getmembers(featureFunction):
+                            if inspect.isclass(obj) and name != 'Base' and not name in excludeList:
+                                # if set(obj().Data).issubset(self.Data):
+                                self.featureOrder.append((inspect.getsourcelines(obj)[-1:])[0])
+                                self.featureList.append(name)
+
+                else:
+                    for feature in featureList:
+                        for name, obj in inspect.getmembers(featureFunction):
+                            if name != 'Base':
+                                if inspect.isclass(obj) and feature == name:
+                                    self.featureList.append(name)
+
+            else:
+
+                if featureList is None:
+                    for name, obj in inspect.getmembers(featureFunction):
+                        if inspect.isclass(obj) and name != 'Base' and not name in excludeList:
+                            if name in kwargs.keys():
+                                if set(obj(kwargs[name]).Data).issubset(self.Data):
+                                    self.featureOrder.append((inspect.getsourcelines(obj)[-1:])[0])
+                                    self.featureList.append(name)
+
+                            else:
+                                if set(obj().Data).issubset(self.Data):
+                                    self.featureOrder.append((inspect.getsourcelines(obj)[-1:])[0])
+                                    self.featureList.append(name)
+                                else:
+                                    msg = "the feature {} could not be calculated because {} are needed.".format(name, obj().Data)
+                                    warnings.warn(msg, FeatureWarning)
+
+                else:
+
+                    for feature in featureList:
+                        for name, obj in inspect.getmembers(featureFunction):
+                            if name != 'Base':
+                                if inspect.isclass(obj) and feature == name:
+                                    if set(obj().Data).issubset(self.Data):
+                                        self.featureList.append(name)
+                                    else:
+                                        msg = "the feature {} could not be calculated because {} are needed.".format(name, obj().Data)
+                                        warnings.warn(msg, FeatureWarning)
+
+            if self.featureOrder != []:
+                self.sort = True
+                self.featureOrder = np.argsort(self.featureOrder)
+                self.featureList = [self.featureList[i] for i in self.featureOrder]
+                self.idx = np.argsort(self.featureList)
+
+        else:
+            self.featureList = featureList
+
+        m = featureFunction
+
+        for item in self.featureList:
+            if item in kwargs.keys():
+                try:
+                    a = getattr(m, item)(kwargs[item])
+                except:
+                    raise FeaturError(item)
+            else:
+                try:
+                    a = getattr(m, item)()
+                except:
+                    raise FeatureNotFound(item)
+            try:
+                self.featureFunc.append(a.fit)
+            except:
+                msg = "could not initilize " + item
+                warnings.warn(msg, FeatureWarning)
+
+    def _make_result(self, result):
+        if self.sort == True:
+            fnames, fvalues = [], []
+            for idx in self.idx:
+                fnames.append(self.featureList[idx])
+                fvalues.append(result[idx])
+        else:
+            fnames = self.featureList
+            fvalues = result
+        return np.asarray(fnames), np.asarray(fvalues)
+
+    def calculateFeature(self, data):
+        X, result = np.asarray(data), []
+        for f in self.featureFunc:
+            result.append(f(X))
+        return self._make_result(result)
