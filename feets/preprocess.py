d382b0efdce5788fdebb9a0bfe7219c6f469368f
Juan B Cabral <jbc.develop@gmail.com>
2017-09-22 01:47:45 -0300

    done

--- feets/preprocess.py
+++ feets/preprocess.py
@@ -30,6 +30,8 @@
 
 import numpy as np
 
+import pandas as pd
+
 from six.moves import range
 
 __all__ = [
@@ -87,61 +89,27 @@ def align(time, time2, mag, mag2, error, error2):
 
     """
 
-    mjd, mjd2 = time, time2  # TODO: use time instead of mjd
-    data, data2 = mag, mag2  # TODO: use mag instead of data
+    error = np.zeros(time.shape) if error is None else error
+    error2 = np.zeros(time2.shape) if error2 is None else error2
 
-    if len(data2) > len(data):
+    # this asume that the first series is the short one
+    sserie = pd.DataFrame({"mag": mag, "error": error}, index=time)
+    lserie = pd.DataFrame({"mag": mag2, "error": error2}, index=time2)
 
-        new_data2 = []
-        new_error2 = []
-        new_mjd2 = []
-        new_mjd = np.copy(mjd)
-        new_error = np.copy(error)
-        new_data = np.copy(data)
-        count = 0
+    # if the second serie is logest then revert
+    if len(time) > len(time2):
+        sserie, lserie = lserie, sserie
 
-        for index in range(len(data)):
+    # make the merge
+    merged = sserie.join(lserie, how="inner", rsuffix='2')
 
-            where = np.where(mjd2 == mjd[index])
+    # recreate columns
+    new_time = merged.index.values
+    new_mag, new_mag2 = merged.mag.values, merged.mag2.values
+    new_error, new_error2 = merged.error.values, merged.error.values
 
-            if np.array_equal(where[0], []) is False:
+    if len(time) > len(time2):
+        new_mag, new_mag2 = new_mag2, new_mag
+        new_error, new_error2 = new_error2, new_error
 
-                new_data2.append(data2[where])
-                new_error2.append(error2[where])
-                new_mjd2.append(mjd2[where])
-            else:
-                new_mjd = np.delete(new_mjd, index - count)
-                new_error = np.delete(new_error, index - count)
-                new_data = np.delete(new_data, index - count)
-                count = count + 1
-
-        new_data2 = np.asarray(new_data2).flatten()
-        new_error2 = np.asarray(new_error2).flatten()
-
-    else:
-
-        new_data = []
-        new_error = []
-        new_mjd = []
-        new_mjd2 = np.copy(mjd2)
-        new_error2 = np.copy(error2)
-        new_data2 = np.copy(data2)
-        count = 0
-        for index in range(len(data2)):
-            where = np.where(mjd == mjd2[index])
-
-            if np.array_equal(where[0], []) is False:
-                new_data.append(data[where])
-                new_error.append(error[where])
-                new_mjd.append(mjd[where])
-            else:
-                new_mjd2 = np.delete(new_mjd2, (index - count))
-                new_error2 = np.delete(new_error2, (index - count))
-                new_data2 = np.delete(new_data2, (index - count))
-                count = count + 1
-
-        new_data = np.asarray(new_data).flatten()
-        new_mjd = np.asarray(new_mjd).flatten()
-        new_error = np.asarray(new_error).flatten()
-
-    return new_mjd, new_data, new_data2, new_error, new_error2
+    return new_time, new_mag, new_mag2, new_error, new_error2
