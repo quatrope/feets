10ef238db0c36a903526c84742748feb12b2208c
Juan B Cabral <jbc.develop@gmail.com>
2017-12-20 01:32:05 -0300

    extractors to menu

--- doc/source/_static/custom.js
+++ doc/source/_static/custom.js
@@ -12,17 +12,12 @@ $(document).ready(function(){
         var title = $div.find("span.extractor-name").text().trim();
         var id = $div.find("div.panel-heading").attr("id");
 
-
-
         $ulFeaturesList.append("<li><a href='#" + id + "'>" + title + "</a></li>");
         $ulFeaturesList.find("a[href='#" + id + "']").click(function(){
             $allPanels.not($panel).collapse("hide");
             $panel.collapse('show');
             window.location.hash = "#" + id;
         });
-
-
-
     });
 
 });
