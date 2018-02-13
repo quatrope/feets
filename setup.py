cab34fb7a87e37acc5dca31164a9521c18ec630e
Juan B Cabral <jbc.develop@gmail.com>
2018-02-13 14:44:09 -0300

    py36 and pytest

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ import feets
 # =============================================================================
 
 REQUIREMENTS = [
-    "numpy", "scipy", "six",
+    "numpy", "scipy", "six", "pytest",
     "statsmodels", "mock", "astropy",
     "pandas", "requests", "attrs"]
 
